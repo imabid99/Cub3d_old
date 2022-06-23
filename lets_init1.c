01b76b312c38873bb9b3a35869487b2cc9e27228
Imad Abid <imabid@student.1337.ma>
2022-06-23 14:47:18 +0100

    Cub3d

--- lets_init1.c
+++ lets_init1.c
@@ -6,7 +6,7 @@
 /*   By: imabid <imabid@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/15 14:50:51 by imabid            #+#    #+#             */
-/*   Updated: 2022/06/23 09:57:53 by imabid           ###   ########.fr       */
+/*   Updated: 2022/06/23 14:46:40 by imabid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,15 +28,14 @@ void    init_all2(t_conf *conf)
 	conf->ray.nextvery = 0;
 	conf->ray.foundhorwallhit = 0;
 	conf->ray.foundverwallhit = 0;
-	
-	// conf->ray.distance = 0;
-	// conf->ray.facingdown = 0;
-	// conf->ray.facingup = 0;
-	// conf->ray.facingright = 0;
-	// conf->ray.facingleft = 0;
+	conf->ray.distance = 0;
+	conf->ray.facingdown = 0;
+	conf->ray.facingup = 0;
+	conf->ray.facingright = 0;
+	conf->ray.facingleft = 0;
 	conf->ray.verdist = 0;
 	conf->ray.hordist = 0;
-	// conf->ray.wallhitX = 0;
-	// conf->ray.wallhitY = 0;
+	conf->ray.wallhitX = 0;
+	conf->ray.wallhitY = 0;
 	conf->ray.washitvert = 0;
 }
